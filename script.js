d671b282996581effcc4898dcef8fbf0cf7499f9
senorwigglezz <erikvaughn33@icloud.com>
2021-02-28 23:14:38 -0700

    Changes to everything/final

--- script.js
+++ script.js
@@ -1,57 +1,133 @@
-const startButton = document.getElementById("start-btn");
-const nextButton = document.getElementById("next-btn");
-const questionsBoxEL = document.getElementById("questionsBox");
-const questionElement = document.getElementById("question");
-const answerBtnsElement = document.getElementById("answer-btns");
-
-var score= 0;
-
-let shuffledQuestions, currentQuestionIndex;
-
-const questions = [
-    {
-        question: "Where is Petaluma?",
-        answers: [
-            { text: "Spain", correct: false },
-            { text: "Brazil", correct: false },
-            { text: "California", correct: true },
-            { text: "Texas", correct: false }
-        ]
-    },
-    {
-        question: "Where is Dublin?",
-        answers: [
-            { text: "Scotland", correct: false },
-            { text: "Ireland", correct: true },
-            { text: "Germany", correct: false },
-            { text: "Austria", correct: false }
-        ]
-    },
-    {
-        question: "Where is The Bean Monument?",
-        answers: [
-            { text: "Austin, Texas", correct: false },
-            { text: "Orlando, Florida", correct: false },
-            { text: "Mars", correct: false },
-            { text: "Chicago, Illinois", correct: true }
-        ]
-    },
-    {
-        question: "Where is the Sydney Opera House?",
-        answers: [
-            { text: "Sydney, Australia", correct: true },
-            { text: "Norway", correct: false },
-            { text: "Moscow, Russia", correct: false },
-            { text: "London, England", correct: false}
-        ]
+(function(){
+    function buildQuiz(){
+      const output = [];
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+          const answers = [];
+          for(letter in currentQuestion.answers){
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
+  
+          output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+          );
+        }
+      );
+      quizContainer.innerHTML = output.join('');
     }
-]
+  
+    function showResults(){
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+      let numCorrect = 0;
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-startButton.addEventListener("click",startGame);
+        if(userAnswer === currentQuestion.correctAnswer){
 
-function startTimer(duration, display) {
+          numCorrect++;
+          answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else{
+          answerContainers[questionNumber].style.color = 'red';
+        }
+      });
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    function showSlide(n) {
+      slides[currentSlide].classList.remove('active-slide');
+      slides[n].classList.add('active-slide');
+      currentSlide = n;
+      if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+      }
+      else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+      }
+    }
+  
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
+
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "Where is Petaluma?",
+        answers: {
+          a: "Spain",
+          b:"Brazil",
+          c:"California",
+          d:"Texas"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Where is Dublin?",
+        answers: {
+            a:"Scotland",
+            b:"Ireland",
+            c:"Germany",
+            d:"Austria"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Where is The Bean Monument?",
+        answers: {
+            a:"Austin, Texas",
+            b:"Orlando, Florida",
+            c:"Mars",
+            d:"Chicago, Illinois"
+        },
+        correctAnswer: "d"
+      },
+      {
+        question: "Where is the Sydney Opera House?",
+        answers: {
+            a:"Sydney, Australia",
+            b:"Norway",
+            c:"Moscow, Russia",
+            d:"London, England"
+        },
+        correctAnswer: "a"
+      }
+    ];
+  
+    buildQuiz();
+
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    showSlide(currentSlide);
+
+    submitButton.addEventListener('click', showResults);
+    nextButton.addEventListener("click", showNextSlide);
+  })();
+
+  function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    setInterval(function() {
+    setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -63,13 +139,14 @@ function startTimer(duration, display) {
         if (--timer < 0) {
             timer = duration;
         }
-        if (seconds === 0) {
-            alert("Game Over!")
+        if(seconds === 0) {
+            alert('Game Over!')
         }
     }, 1000);
 }
-
-startButton.addEventListener("click",startGame);
+startButton.addEventListener("click", startTime);
 startButton.addEventListener("click", function() {
-    
-})
+    var oneMinute = 60,
+        display = document.querySelector('#clock');
+    startTimer(oneMinute, display);
+});
